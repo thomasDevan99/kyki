6bb415664d7af60f1925593c181c2a70a1612f15
Devan Thomas <thomasdevan99@gmail.com>
2025-04-11 15:05:24 -0400

    Hide btn after press, show text above reveal

--- my-strapi-project/kyki/src/app/page.js
+++ my-strapi-project/kyki/src/app/page.js
@@ -61,11 +61,13 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
             
 
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+      <main className="gap-[32px] row-start-2 items-center sm:items-start">
+      
+      <div className="pb-56 sm:items-center justify-items-center">
+        {pulled && pullText}
+      </div>
 
-    
         <div className="sm:items-center justify-items-center">
-              {pullText}
               {pulled && <RotateShowcase 
               show={pulled?.rarityNum}
               pulledRarity={pulled.shortName}
@@ -75,7 +77,7 @@ export default function Home() {
           <div className="flex items-center flex-col sm:flex-row">
             
             
-            <button
+            {!pulled && <button
               className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
               target="_blank"
               onClick={() => pullBox()}
@@ -89,7 +91,7 @@ export default function Home() {
                 height={20}
               />
               Pull Now
-            </button>
+            </button>}
             
           </div>
         </div>
