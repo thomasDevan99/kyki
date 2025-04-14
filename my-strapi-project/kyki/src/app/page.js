bab648404d92829470d58ca535c20a5d85cd11f4
Devan Thomas <thomasdevan99@gmail.com>
2025-04-14 16:45:21 -0400

    center all content

--- my-strapi-project/kyki/src/app/page.js
+++ my-strapi-project/kyki/src/app/page.js
@@ -10,10 +10,9 @@ export default function Home() {
 
 
   return (
-    <div className="grid grid-rows-[auto_1fr_auto] min-h-screen items-center justify-items-center px-4 sm:px-8 md:px-12 py-8 font-[family-name:var(--font-geist-sans)]">
+    <div className="content-center min-h-screen items-center justify-items-center px-4 sm:px-8 md:px-12 py-8 font-[family-name:var(--font-geist-sans)]">
       <main className="w-full max-w-4xl flex flex-col items-center">
         {!isIdle && <RotateShowcase setisIdle={setisIdle} />}
-        
         {isIdle && (
           <motion.button
             className="rounded-full border border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-6"
