cc3c80c237df374d73fed394b0ee6ac6a4662d60
Devan Thomas <thomasdevan99@gmail.com>
2025-04-22 17:29:44 -0400

    Remove default recursive file paths

---
+++ src/app/page.js
@@ -0,0 +1,44 @@
+'use client';
+
+import Image from "next/image";
+import { useState } from "react";
+import RotateShowcase from "./rotateShowcase";
+import { motion } from "framer-motion";
+
+export default function Home() {
+  const [isIdle, setisIdle] = useState(true);
+
+
+  return (
+    <div className="content-center min-h-screen items-center justify-items-center px-4 sm:px-8 md:px-12 py-8 font-[family-name:var(--font-geist-sans)]">
+      <main className="w-full max-w-4xl flex flex-col items-center">
+        {!isIdle && <RotateShowcase setisIdle={setisIdle} />}
+        {isIdle && (
+          <motion.button
+            className="rounded-full border border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-6"
+            onClick={() => setisIdle(false)}
+            animate={{
+              y: [0, -4, 0],       // float up and back down
+              scale: [1, 1.05, 1], // slight breathing effect
+            }}
+            transition={{
+              duration: 2,
+              ease: "easeInOut",
+              repeat: Infinity,
+              repeatType: "loop",
+            }}
+          >
+            <Image
+              className="dark:invert"
+              src="/vercel.svg"
+              alt="Vercel logomark"
+              width={20}
+              height={20}
+            />
+            Pull Now
+          </motion.button>
+        )}
+      </main>
+    </div>
+  );
+}
---
+++ src/dataStructure/possibleResults.js
@@ -0,0 +1,36 @@
+// default
+// Common - 50
+// Uncommon - 25
+// Rare - 20
+// Super Rare - 5
+
+export const possibleResults = [
+    {
+    _id: 1,
+    rarityNum: 1,
+    rarityName: "Common",
+    shortName: "C",
+    chance: 50
+    },
+    {
+    _id: 2,
+    rarityNum: 2,
+    rarityName: "Uncommon",
+    shortName: "UC",
+    chance: 25
+    },
+    {
+    _id: 3,
+    rarityNum: 3,
+    rarityName: "Rare",
+    shortName: "R",
+    chance: 20
+    },
+    {
+    _id: 4,
+    rarityNum: 4,
+    rarityName: "Super Rare",
+    shortName: "SR",
+    chance: 5
+    },
+  ]
