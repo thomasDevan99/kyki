dd855aaa888ff18776f16fac1668635d686d988d
Devan Thomas <thomasdevan99@gmail.com>
2025-04-23 09:31:06 -0400

    Update: Remove C rank and shift all other possibleResults

--- src/dataStructure/possibleResults.js
+++ src/dataStructure/possibleResults.js
@@ -9,41 +9,33 @@ export const possibleResults = [
     _id: 1,
     rarityNum: 1,
     rarityName: "Common",
-    shortName: "C",
-    chance: 50,
-    color: "#9CA3AF"
+    shortName: "B",
+    chance: 87,
+    color: "#60A5FA"
     },
     {
     _id: 2,
     rarityNum: 2,
     rarityName: "Uncommon",
-    shortName: "B",
-    chance: 37,
-    color: '#60A5FA'
+    shortName: "A",
+    chance: 10,
+    color: '#C084FC'
     },
     {
     _id: 3,
     rarityNum: 3,
     rarityName: "Rare",
-    shortName: "A",
-    chance: 10,
-    color: '#C084FC'
+    shortName: "S",
+    chance: 2,
+    color: '#FB923C'
 
     },
     {
     _id: 4,
     rarityNum: 4,
     rarityName: "Super Rare",
-    shortName: "S",
-    chance: 2,
-    color: '#FB923C'
+    shortName: "SS",
+    chance: 1,
+    color: '#FACC15'
     },
-    {
-      _id: 5,
-      rarityNum: 5,
-      rarityName: "Legendary",
-      shortName: "SS",
-      chance: 1,
-      color: '#FACC15'
-    }
   ]
