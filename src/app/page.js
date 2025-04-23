0681238d1d67ad60ef1583e96b70038322780e04
Devan Thomas <thomasdevan99@gmail.com>
2025-04-23 09:24:06 -0400

    Add: Gap to pull buttons, setPullNum to single pull

--- src/app/page.js
+++ src/app/page.js
@@ -11,12 +11,15 @@ export default function Home() {
 
   return (
     <div className="content-center min-h-screen items-center justify-items-center px-4 sm:px-8 md:px-12 py-8 font-[family-name:var(--font-geist-sans)]">
-      <main className="w-full max-w-4xl flex flex-col items-center">
+      <main className="w-full max-w-4xl flex flex-col items-center gap-4">
         {!isIdle && <RotateShowcase setisIdle={setisIdle} numberOfPulls={pullNum} />}
         {isIdle && (
           <motion.button
             className="rounded-full border border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-6"
-            onClick={() => setisIdle(false)}
+            onClick={() => {
+              setisIdle(false)
+              setPullNum(1)
+            }}
             animate={{
               y: [0, -4, 0],       // float up and back down
               scale: [1, 1.05, 1], // slight breathing effect
