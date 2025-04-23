8bb3f09923a36d7b0576cb1c75a6a9efe067eece
Devan Thomas <thomasdevan99@gmail.com>
2025-04-23 09:25:22 -0400

    Clean up multipull, and use components in showcase

--- src/app/animations/multipullRotate.js
+++ src/app/animations/multipullRotate.js
@@ -1,120 +1,96 @@
 import React, { useState } from "react";
 import { motion } from "framer-motion";
-import { genFakePull, pullBox } from "../helpers/pullMethods";
+import { genFakePull } from "../helpers/pullMethods";
+
 const buttons = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];  // 10 buttons
 const radius = 150;  // Radius of the orbit
 
-export default function MultipullRotate({ setisIdle, numberOfPulls }) {
-  const [flipped, setFlipped] = useState(false);
+export default function MultipullRotate({ flipped, setFlipped }) {
 
-  // Handle button click (flip and select behavior)
-  const handleButtonClick = () => {
-    setFlipped((prev) => {
-        if (prev) return prev
-        return !prev;
-      }); 
-  };
-
-  // Reset the portal and button states
-  const reset = () => {
-    setFlipped(false);
-    setisIdle(true);
-  };
-
-  // Animation for each orbiting button
-  const animationProps = (id, flipped, color) => {
-    const angle = (id / buttons.length) * 2 * Math.PI;
-    const x = radius * Math.cos(angle);
-    const y = radius * Math.sin(angle);
-
-    return {
-      x: x,
-      y: y,
-      scale: flipped ? 1.3 : 1,
-      zIndex: flipped ? 10 : 1,
-      rotateY: flipped ? 1080 : 0,
-      backgroundColor: ["#9CA3AF", color],
+    // Handle button click (flip and select behavior)
+    const handleButtonClick = () => {
+        setFlipped((prev) => {
+            if (prev) return prev
+            return !prev;
+        }); 
     };
-  };
 
-  // Transition props for animation
-  const transitionProps = (flipped) => ({
-    type: "spring",
-    stiffness: flipped ? 10 : 100,
-    damping: flipped ? 3 : 20,
-  });
+    // Animation for each orbiting button
+    const animationProps = (id, flipped, color) => {
+        const angle = (id / buttons.length) * 2 * Math.PI;
+        const x = radius * Math.cos(angle);
+        const y = radius * Math.sin(angle);
+
+        return {
+        x: x,
+        y: y,
+        scale: flipped ? 1.3 : 1,
+        zIndex: flipped ? 10 : 1,
+        rotateY: flipped ? 1080 : 0,
+        backgroundColor: ["#9CA3AF", color],
+        };
+    };
+
+    // Transition props for animation
+    const transitionProps = (flipped) => ({
+        type: "spring",
+        stiffness: flipped ? 10 : 100,
+        damping: flipped ? 3 : 20,
+    });
   
-  return (
-    <>
-      {/* Orbiting buttons and central button */}
-      <div className="relative w-full h-[80vh] flex items-center justify-center overflow-hidden">
-        {/* Orbiting Buttons Container */}
-        <motion.div
-          className="absolute w-full h-full flex justify-center items-center"
-          animate={{ rotate: 360 }}
-          transition={{
-            repeat: Infinity,
-            duration: 8,
-            ease: "linear",
-          }}
-        >
-          {/* Orbiting Buttons */}
-          {buttons.map((id) => {
-            const rarityFake = genFakePull(flipped);
-            const color = flipped && rarityFake?.color
+    return (
+        <>
+            {/* Orbiting buttons and central button */}
+            <div className="relative w-full h-[80vh] flex items-center justify-center overflow-hidden">
+            {/* Orbiting Buttons Container */}
+            <motion.div
+            className="absolute w-full h-full flex justify-center items-center"
+            animate={{ rotate: 360 }}
+            transition={{
+                repeat: Infinity,
+                duration: 8,
+                ease: "linear",
+            }}
+            >
+            {/* Orbiting Buttons */}
+            {buttons.map((id) => {
+                const rarityFake = genFakePull(flipped);
+                const color = flipped && rarityFake?.color
 
-            return (
-              <motion.button
-                key={id}
-                className="absolute w-12 h-12 sm:w-16 sm:h-16 rounded-full bg-gray-400 text-white flex items-center justify-center"
-                animate={animationProps(id, flipped, color)}
-                transition={transitionProps(flipped)}
-              >
-                <motion.div
-                  style={{ perspective: 1000 }}
-                  className="w-full h-full flex items-center justify-center"
+                return (
+                <motion.button
+                    key={id}
+                    className="absolute w-12 h-12 sm:w-16 sm:h-16 rounded-full bg-gray-400 text-white flex items-center justify-center"
+                    animate={animationProps(id, flipped, color)}
+                    transition={transitionProps(flipped)}
                 >
-                  <div className="w-full h-full backface-hidden text-black flex items-center justify-center">
-                    {flipped ? rarityFake.shortName : "?"}
-                  </div>
-                </motion.div>
-              </motion.button>
-            );
-          })}
-        </motion.div>
+                    <motion.div
+                    style={{ perspective: 1000 }}
+                    className="w-full h-full flex items-center justify-center"
+                    >
+                    <div className="w-full h-full backface-hidden text-black flex items-center justify-center">
+                        {flipped ? rarityFake.shortName : "?"}
+                    </div>
+                    </motion.div>
+                </motion.button>
+                );
+            })}
+            </motion.div>
 
-        {/* Central Button */}
-        <motion.button
-          className="absolute w-16 h-16 rounded-full bg-blue-500 text-white flex items-center justify-center"
-          onClick={() => {
-            setFlipped(!flipped)
-            handleButtonClick()
-        }}  // Toggle flip on center button click
-          animate={{ scale: flipped ? 1.2 : 1 }}
-          transition={{ type: "spring", stiffness: 200, damping: 20 }}
-        >
-          {flipped ? "PULL" : "Center"}  {/* Change text based on flip state */}
-        </motion.button>
-      </div>
+            {/* Central Button */}
+            {!flipped && <motion.button
+            className="absolute w-16 h-16 rounded-full bg-blue-500 text-white flex items-center justify-center"
+            onClick={() => {
+                setFlipped(!flipped)
+                handleButtonClick()
+            }}  // Toggle flip on center button click
+            animate={{ scale: flipped ? 1.2 : 1 }}
+            transition={{ type: "spring", stiffness: 200, damping: 20 }}
+            >
+                PULL
+            </motion.button>}
+        </div>
 
-      {/* Action Buttons (Pull Again & Reveal All) */}
-      <div className="sm:items-center justify-items-center grid grid-cols-2 gap-10">
-        {(
-          <motion.button
-            className="rounded-full bg-white text-black flex items-center justify-center"
-            onClick={() => reset()}
-          >
-            <div className="p-4">Pull Again</div>
-          </motion.button>
-        )}
-        {(
-          <motion.button
-            className="rounded-full bg-white text-black flex items-center justify-center"
-          >
-            <div className="p-4">Reveal All</div>
-          </motion.button>
-        )}
-      </div>
-    </>
-  );
+        </>
+    );
 }
--- src/app/rotateShowcase.js
+++ src/app/rotateShowcase.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { motion } from "framer-motion";
 import { genFakePull, pullBox } from "./helpers/pullMethods";
+import MultipullRotate from "./animations/multipullRotate";
+import SingleSelect from "./animations/singleSelect";
 
 const buttons = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
@@ -10,20 +12,8 @@ export default function OrbitingPortal({ setisIdle, numberOfPulls }) {
   const [flipped, setFlipped] = useState(false);
   const [pulled, setPulled] = useState();
   const [allReveal, setAllReveal] = useState(false)
-  console.log('numberOfPulls', numberOfPulls);
+
   
-  const handleButtonClick = (id, numberOfPulls) => {
-    console.log('selected', selected);
-   
-    if ((selected === null) && (numberOfPulls === 1)) {
-      setSelected(id);
-    } else if (selected === id || numberOfPulls != 1) {
-      setFlipped((prev) => {
-        if (prev) return prev
-        return !prev;
-      });
-    }
-  };
 
   const reset = () => {
     setSelected(null)
@@ -33,48 +23,6 @@ export default function OrbitingPortal({ setisIdle, numberOfPulls }) {
     setAllReveal(false)
   }
 
-  const animationProps = (id, isSelected, isRevealed, color) => {
-    const angle = (id / buttons.length) * 2 * Math.PI;
-    const x = radius * Math.cos(angle);
-    const y = radius * Math.sin(angle);
-    if (!flipped) {
-
-      return {
-        x: isSelected ? 0 : x,
-        y: isSelected ? 0 : y,
-        scale: isSelected ? 1.3 : 1,
-        zIndex: isSelected ? 10 : 1,
-        rotateY: isRevealed ? 1080 : 0,
-      };
-    } else {
-
-      return {
-        scale: [1, 1.05, 1],
-        x: isSelected ? 0 : x,
-        y: isSelected ? 0 : y,
-        rotateY: isRevealed ? 1080 : 0,
-        backgroundColor: ["#9CA3AF", color],
-      }
-    }
-  };
-
-  const transitionProps = (isRevealed) => {
-    if (!flipped) {
-      return {
-      type: "spring",
-      stiffness: isRevealed ? 10 : 100,
-      damping: isRevealed ? 3 : 20,
-      }
-    } else {
-        return {
-          duration: 2,
-          ease: "easeInOut",
-          repeat: 0,
-          repeatType: "loop",
-        }
-    }
-  };
-
   const pullText = selected != null ? `YOU GOT A ${pulled?.rarityName} REWARD` : 'You have not pulled for an item yet';
 
   const radius = typeof window !== "undefined"
@@ -83,62 +31,13 @@ export default function OrbitingPortal({ setisIdle, numberOfPulls }) {
       : 150
     : 150;
 
+
   return (
     <>
-      
       <div className="relative w-full h-[80vh] flex items-center justify-center overflow-hidden">
-        {/* Orbiting Buttons */}
-        {buttons.map((id, index) => {
-          const angle = (index / buttons.length) * 2 * Math.PI;
-          const x = radius * Math.cos(angle);
-          const y = radius * Math.sin(angle);
-      
-
-          const isSelected = selected === id;
-          const isRevealed = isSelected && flipped || allReveal;
-
-
-          const rarityFake = genFakePull(allReveal, flipped)
-
-          const color = isSelected ? pulled?.color : allReveal && rarityFake?.color
-
-          return (
-            // <motion.button
-            //   key={id}
-            //   className="absolute w-12 h-12 sm:w-16 sm:h-16 rounded-full bg-green-400 text-white flex items-center justify-center"
-            //   onClick={() => handleButtonClick(id)}
-            //   animate={animationProps(id, isSelected, isRevealed)}
-            //   transition={transitionProps(isRevealed)}
-            // >
-            //   <motion.div
-            //     style={{ perspective: 1000 }}
-            //     onClick={() => isSelected && pullBox()}
-            //     className="w-full h-full flex items-center justify-center"
-            //   >
-            //     <div className="w-full h-full backface-hidden text-black flex items-center justify-center">
-            //       {isSelected && flipped ? pulled?.shortName : allReveal ? genFakePull() : "?"}
-            //     </div>
-            //   </motion.div>
-            // </motion.button>
-            <motion.button
-              key={id}
-              className="absolute w-12 h-12 sm:w-16 sm:h-16 rounded-full bg-gray-400 text-white flex items-center justify-center"
-              onClick={() => handleButtonClick(id, numberOfPulls)}
-              animate={animationProps(id, isSelected, isRevealed, color)}
-              transition={transitionProps(isRevealed)}
-            >
-              <motion.div
-                style={{ perspective: 1000 }}
-                onClick={() => isSelected && pullBox(pulled, setPulled)}
-                className="w-full h-full flex items-center justify-center"
-              >
-                <div className="w-full h-full backface-hidden text-black flex items-center justify-center">
-                  {isSelected && flipped ? pulled?.shortName : allReveal ? rarityFake.shortName : "?"}
-                </div>
-              </motion.div>
-            </motion.button>
-          );
-        })}
+        {numberOfPulls === 1 ? 
+        <SingleSelect allReveal={allReveal} pulled={pulled} setPulled={setPulled} flipped={flipped} setFlipped={setFlipped} selected={selected} setSelected={setSelected}/> : 
+        <MultipullRotate flipped={flipped} setFlipped={setFlipped} /> }       
       </div>
 
       <div className="text-center text-base sm:text-lg">
@@ -146,7 +45,7 @@ export default function OrbitingPortal({ setisIdle, numberOfPulls }) {
       </div>
 
       <div className="sm:items-center justify-items-center grid grid-cols-2 gap-10">
-        {pulled &&
+        {(pulled || numberOfPulls == 10 && flipped) &&
         <motion.button className="rounded-full bg-white text-black flex items-center justify-center" 
           onClick={() => reset()}
         >
