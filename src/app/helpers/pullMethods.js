74cc399cae3ce098dc032fa467eb24308e1b5f0d
Devan Thomas <thomasdevan99@gmail.com>
2025-08-11 22:40:00 -0400

    Pull method start rework

--- src/app/helpers/pullMethods.js
+++ src/app/helpers/pullMethods.js
@@ -13,16 +13,51 @@ export const pullBox = (pullData, setPullData) => {
     temp && setPullData(temp)
   };
 
-  export const genFakePull = (allReveal, ogRevealed) => {
-    if (!allReveal && !ogRevealed) return
+export async function genFakePull() {
+  const pullNum = Math.random() * 100;
 
-    const pullNum = Math.random() * 100;
-    
-     const temp = possibleResults.find((res, index) => {
-      const minChance = index === 0 ? 0 : possibleResults.slice(0, index).reduce((acc, val) => acc + val.chance, 0);
-      const maxChance = minChance + res.chance;
-      return pullNum >= minChance && pullNum < maxChance;
-    });
+  const temp = possibleResults.find((res, index) => {
+    const minChance =
+      index === 0
+        ? 0
+        : possibleResults
+            .slice(0, index)
+            .reduce((acc, val) => acc + val.chance, 0);
+    const maxChance = minChance + res.chance;
+    return pullNum >= minChance && pullNum < maxChance;
+  });
 
-    return temp
+  return temp;
+}
+
+
+  // Generate N pulls at once
+export async function genMultiplePulls(count = 10) {
+  const pulls = [];
+  for (let i = 0; i < count; i++) {
+    const pull = await genFakePull();
+    pulls.push(pull);
   }
+
+  // if 10 pulls or more
+  if (count >= 10) {
+    const hasHighRank = pulls.some(pull =>
+    ["A", "S", "SS"].includes(pull.shortName)
+    )
+
+    //Make at least 1 an A rank
+    if (!hasHighRank) {
+      const aRankItems = possibleResults.filter(item => item.shortName === "A");
+      
+      const randomIndex = Math.floor(Math.random() * aRankItems.length);
+      
+      const pullReplacement = aRankItems[randomIndex];
+      const randomIndexReplace = Math.floor(Math.random() * count)
+      pulls[randomIndexReplace] = pullReplacement
+
+    }
+  }
+
+  return pulls;
+}
+
